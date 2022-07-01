4f216c5333860a16f01b7db2abf70fc1e8aece9c
isaac herrera yepes <isaacheye@unisabana.edu.co>
2022-07-01 12:57:18 -0500

    minor style changes

--- subpages/projects/projects.js
+++ subpages/projects/projects.js
@@ -1,2 +1,15 @@
-const dialog = document.querySelector("dialog")
-const show = document.querySelector("#show")
+//urls
+const MYADDRESSVALIDATOR_URL = "https://myaddress-validator.web.app/"
+const EXHIBITION_MYADDRESSVALIDATOR = "./myaddressValidator/myaddressvalidator.html"
+//object that holds the urls so its easy accesing them
+const links = {
+    "MYADDRESSVALIDATOR_URL":MYADDRESSVALIDATOR_URL,
+    "EXHIBITION_MYADDRESSVALIDATOR":EXHIBITION_MYADDRESSVALIDATOR
+}
+
+// putting event to all buttons with class url, their id must correspont to a constant in urls above
+const buttons = document.querySelectorAll(".Url")
+const subscribeUrl= button => {
+    button.addEventListener("click",()=> window.location.href = links[button.id])
+}
+buttons.forEach(subscribeUrl)
